8160527cbcb074f9c47ba1ae7f557a8ff1863771
Luiz Lopes <luizclopes.soares@gmail.com>
2024-02-17 08:31:53 -0300

    Nicknames

---
+++ part08-Part08_06.Nicknames/src/main/java/Nicknames.java
@@ -0,0 +1,15 @@
+
+import java.util.HashMap;
+
+public class Nicknames {
+
+    public static void main(String[] args) {
+        HashMap<String,String> hashMap = new HashMap<>();
+        
+        hashMap.put("matthew", "matt");
+        hashMap.put("michael", "mix");
+        hashMap.put("arthur","artie");
+        
+        System.out.println(hashMap.get("matthew"));
+    }
+}
