23de5f6846a80c877200ed951dda6ee3f7a885a0
Luiz Lopes <luizclopes.soares@gmail.com>
2024-03-04 01:03:17 -0300

    Average of selected numbers

---
+++ part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
@@ -0,0 +1,46 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class AverageOfSelectedNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<String> list = new ArrayList<>();
+        
+        System.out.println("Input numbers, type \"end\" to stop.");
+        
+        while(true){
+            String input = scanner.nextLine();
+            
+            if(input.equals("end")){
+                break;
+            }
+            
+            list.add(input);
+        }
+
+        
+        System.out.println("Print the average of the negative numbers or the positive numbers? (n/p)");
+        String input = scanner.nextLine();
+        
+        if(input.equals("n")){
+            double average = list.stream()
+                    .mapToInt(number -> Integer.valueOf(number))
+                    .filter(number -> number < 0)
+                    .average()
+                    .getAsDouble();
+            
+            System.out.println("Average of the negative numbers: " + average);
+        }else{
+            double average = list.stream()
+                    .mapToInt(number -> Integer.valueOf(number))
+                    .filter(number -> number > 0)
+                    .average()
+                    .getAsDouble();
+            
+            System.out.println("Average of the positive numbers: " + average);
+        }
+    }
+}
