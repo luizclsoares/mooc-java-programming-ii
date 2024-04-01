fef14f24ad4d7ba78434e08e36f8c87dd4d4222a
Luiz Lopes <luizclopes.soares@gmail.com>
2024-04-01 12:35:53 -0300

    Hash map

---
+++ part12-Part12_05.HashMap/src/main/java/HashMap.java
@@ -0,0 +1,62 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author luiz
+ */
+public class HashMap<K, V> {
+
+    private List<Pair<K, V>>[] values;
+    private int firstFreeIndex;
+
+    public HashMap() {
+        this.values = new List[32];
+        this.firstFreeIndex = 0;
+    }
+    
+    public V get(K key) {
+        int hashValue = Math.abs(key.hashCode() % this.values.length);
+        if (this.values[hashValue] == null) {
+            return null;
+        }
+
+        List<Pair<K, V>> valuesAtIndex = this.values[hashValue];
+
+        for (int i = 0; i < valuesAtIndex.size(); i++) {
+            if (valuesAtIndex.get(i).getKey().equals(key)) {
+                return valuesAtIndex.get(i).getValue();
+            }
+        }
+
+        return null;
+    }
+    
+    private void copy(List<Pair<K, V>>[] newArray, int fromIdx) {
+        for (int i = 0; i < this.values[fromIdx].size(); i++) {
+            Pair<K, V> value = this.values[fromIdx].get(i);
+ 
+            int hashValue = Math.abs(value.getKey().hashCode() % newArray.length);
+            if (newArray[hashValue] == null) {
+                newArray[hashValue] = new ArrayList<>();
+            }
+ 
+            newArray[hashValue].add(value);
+        }
+    }
+    
+    private void grow() {
+        List<Pair<K, V>>[] newValues = new List[this.values.length * 2];
+
+        copy(newValues, 0);
+
+        this.values = newValues;
+    }
+}
